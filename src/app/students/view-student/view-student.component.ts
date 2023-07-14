e864d5ee8dc014aed40e9ae41a678abe23074fde
wahab <wahab.deve@gmail.com>
2023-07-14 18:22:02 +0500

    End of Section 7

--- src/app/students/view-student/view-student.component.ts
+++ src/app/students/view-student/view-student.component.ts
@@ -37,6 +37,9 @@ export class ViewStudentComponent implements OnInit {
 
 
   }
+
+  isNewStudent= false;
+  header = '';
   genderList: Gender[]=[];
 
   constructor(private readonly studentService: StudentService,
@@ -51,12 +54,31 @@ export class ViewStudentComponent implements OnInit {
       (params)=>{
        this.studentId = params.get('id');
        if(this.studentId){
-        this.studentService.getStudent(this.studentId)
+        
+        if(this.studentId.toLowerCase()==='Add'.toLowerCase())
+        {
+          this.isNewStudent = true;
+          this.header = 'Add New Student';
+
+          //new studnet
+        }
+        else
+        {
+          this.isNewStudent= false;
+          this.header = 'Edit Student';
+          this.studentService.getStudent(this.studentId)
         .subscribe(
           (successResponse)=>{
             this.student = successResponse;
           }
         );
+
+
+          //existing studnet
+        }
+
+        
+        
         this.genderService.getGenderList()
         .subscribe(
           (successResponse)=>{
@@ -98,6 +120,25 @@ export class ViewStudentComponent implements OnInit {
       (errorResponse)=>{
         //log
       }
+    );
+  }
+
+  onAdd(): void {
+    this.studentService.addStudent(this.student)
+    .subscribe
+    (
+      (successResponse)=>
+      {
+        this.snackbar.open('Student added successfully',undefined,{duration:2000});
+        setTimeout(()=>{
+          this.router.navigateByUrl(`students/${successResponse.id}`);
+        },2000);
+      },
+      (errorResponse)=>
+      {
+
+      }
+
     );
   }
 }
