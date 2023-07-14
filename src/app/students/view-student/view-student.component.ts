dfe32adb85be75d3aa31149fa5e59924840f884f
wahab <wahab.deve@gmail.com>
2023-07-14 16:14:25 +0500

    End of Section 6

--- src/app/students/view-student/view-student.component.ts
+++ src/app/students/view-student/view-student.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { StudentService } from '../student.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Student } from 'src/app/models/ui-models/student.model';
 import { GenderService } from 'src/app/services/gender.service';
 import { Gender } from 'src/app/models/ui-models/gender.model';
@@ -38,8 +38,12 @@ export class ViewStudentComponent implements OnInit {
 
   }
   genderList: Gender[]=[];
-  constructor(private readonly studentService: StudentService, private readonly route:ActivatedRoute,
-    private readonly genderService: GenderService, private snackbar:MatSnackBar){}
+
+  constructor(private readonly studentService: StudentService,
+     private readonly route:ActivatedRoute,
+    private readonly genderService: GenderService,
+     private snackbar:MatSnackBar,
+     private router:Router){}
 
 
   ngOnInit(): void{
@@ -79,5 +83,21 @@ export class ViewStudentComponent implements OnInit {
      
       
       );
+  }
+
+  onDelete():void{
+    this.studentService.deleteStudent(this.student.id)
+    .subscribe(
+      (successResponse)=>{
+        this.snackbar.open('Student deleted successfully',undefined,{duration:2000});
+        setTimeout(()=>{
+          this.router.navigateByUrl('students');
+        },2000);
+        
+      },
+      (errorResponse)=>{
+        //log
+      }
+    );
   }
 }
